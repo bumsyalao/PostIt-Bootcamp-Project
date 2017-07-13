641b5a8168d912c255f942d18edcee6ddbbdb15d
bumsyalao <alaobunmi93@gmail.com>
2017-07-13 20:20:48 +0100

    deployed to heroku

--- server/controllers/usergroups.js
+++ server/controllers/usergroups.js
@@ -8,12 +8,18 @@ export default {
     const groupId = Number(req.params.groupid);
     Groups.findById(groupId)
     .then((foundGroup) => {
-      Usergroups.create({
-        groupId: foundGroup.id,
-        userId: req.body.userId,
-      })
-      .then(newGroup => res.status(200).send(newGroup))
-      .catch(error => res.status(400).send(error));
+      Usergroups.findAll({ where: { userId: req.body.userId } })
+        .then((user) => {
+          if (user.length !== 0) {
+            return res.send({ message: 'User already in group' });
+          }
+          Usergroups.create({
+            groupId: foundGroup.id,
+            userId: req.body.userId,
+          })
+          .then(newGroup => res.status(200).send(newGroup))
+          .catch(error => res.status(400).send(error));
+        }).catch(error => res.status(400).send(error));
     });
   },
 };
