6c345c9973dc0469f95ea0815c6204476f619aca
bumsyalao <alaobunmi93@gmail.com>
2017-07-20 14:06:00 +0100

    modified users controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,46 +9,57 @@ const secret = process.env.SECRET;
 export default {
 
   signup(req, res) {
-    Users
-    .create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    })
-    .then((newUser) => {
-      const token = jwt.sign({
-        userId: newUser.id,
-        username: newUser.username,
-        email: newUser.email
-      }, secret, { expiresIn: '1 day' });
-      const userInfo = {
-        userId: newUser.id,
-        username: newUser.username,
-        email: newUser.email
-      };
-      return res.status(200).send({ token, userInfo });
-    })
-    .catch(error => res.status(400).send(error.message));
+    if (req.body.username && req.body.email && req.body.password) {
+      Users
+        .create({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+        })
+        .then((newUser) => {
+          const token = jwt.sign({
+            userId: newUser.id,
+            username: newUser.username,
+            email: newUser.email
+          }, secret, { expiresIn: '1 day' });
+          const userInfo = {
+            userId: newUser.id,
+            username: newUser.username,
+            email: newUser.email
+          };
+          return res.status(200).send({ token, userInfo });
+        })
+        .catch(error => res.status(400).send({ message: error.message }));
+    } else {
+      return res.status(400)
+        .send({ message: 'Incomplete registration details' });
+    }
   },
 
   signin(req, res) {
-    Users.findOne({ where: { username: req.body.username } })
-      .then((foundUser) => {
-        if (foundUser && foundUser.verifyPassword(req.body.password)) {
-          const token = jwt.sign({
-            userId: foundUser.id,
-            username: foundUser.username,
-            email: foundUser.email
-          }, secret, { expiresIn: '1 day' });
-          return res.status(200)
-            .send({
-              token,
-              success: true,
-              message: 'You have logged in succesfully'
-            });
-        }
-        return res.status(401)
-          .send({ success: false, message: 'Incorrect username or password' });
-      });
+    if (req.body.username && req.body.password) {
+      Users.findOne({ where: { username: req.body.username } })
+        .then((foundUser) => {
+          if (foundUser && foundUser.verifyPassword(req.body.password)) {
+            const token = jwt.sign({
+              userId: foundUser.id,
+              username: foundUser.username,
+              email: foundUser.email
+            }, secret, { expiresIn: '1 day' });
+            return res.status(200)
+              .send({
+                token,
+                success: true,
+                message: 'You have logged in succesfully'
+              });
+          }
+          return res.status(401)
+            .send({ success: false, message: 'Incorrect username or password' });
+        })
+        .catch(error => res.status(400).send({ message: error.message }));
+    } else {
+      return res.status(400)
+        .send({ message: 'Incomplete login details' });
+    }
   },
 };
