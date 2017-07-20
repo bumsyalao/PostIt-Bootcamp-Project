704fcd9e00f3c7f2dce140b74f33d980cf73945f
bumsyalao <alaobunmi93@gmail.com>
2017-07-20 13:55:50 +0100

    updated testcases

---
+++ test/models/users.test.js
@@ -0,0 +1,82 @@
+/* eslint-disable no-unused-expressions */
+import chai from 'chai';
+import models from '../../server/models';
+
+const Users = models.Users;
+const expect = chai.expect;
+
+const userInfo = {
+  email: 'power.rangers@super.com',
+  username: 'zack',
+  password: 'alfalfa'
+};
+
+const requiredFields = [
+  'username', 'email', 'password'];
+
+const uniqueFields = ['email', 'username'];
+
+
+describe('User model validation:', () => {
+  after((done) => {
+    Users.destroy({ where: { username: 'zack' } }).then(() => done());
+  });
+
+  let user;
+  beforeEach((done) => {
+    user = Users.build(userInfo);
+    done();
+  });
+
+  describe('Required Fields', () => {
+    requiredFields.forEach((field) => {
+      it(`requires ${field} field to create a user`, () => {
+        user[field] = null;
+        return user.save()
+          .catch((error) => {
+            expect(/notNull Violation/.test(error.message)).to.be.true;
+          });
+      });
+    });
+  });
+
+  describe('Unique Fields', () => {
+    uniqueFields.forEach((field) => {
+      it(`requires ${field} field to be Unique`, () => {
+        user.save()
+          .catch((error) => {
+            expect(/UniqueConstraintError/.test(error.name)).to.be.true;
+          });
+      });
+    });
+  });
+
+  describe('Mail Validation', () => {
+    it('requires user mail to be authentic', () => {
+      user.email = 'sugarbad.com';
+      return user.save()
+        .catch((error) => {
+          expect(/ValidationError/.test(error.name)).to.be.true;
+        });
+    });
+  });
+
+  describe('Password Validation', () => {
+    let person;
+    before((done) => {
+      userInfo.email = 'boye@dollars.com';
+      userInfo.username = 'lala';
+      Users.create(userInfo)
+        .then((createdUser) => {
+          person = createdUser;
+          done();
+        });
+    });
+    after((done) => {
+      Users.destroy({ where: { username: 'lala' } }).then(() => done());
+    });
+    it('should be valid if compared', () => {
+      expect(person.verifyPassword(userInfo.password)).to.be.true;
+    });
+  });
+});
---
+++ test/routes/users.test.js
@@ -0,0 +1,125 @@
+import chai from 'chai';
+import supertest from 'supertest';
+import app from '../../app';
+import models from '../../server/models';
+
+const expect = chai.expect;
+const api = supertest(app);
+
+const chopper = {
+  username: 'candy',
+  email: 'chopper@anime.com',
+  password: 'medicine'
+};
+
+const existingUsername = {
+  username: 'candy',
+  email: 'invalid@test.com',
+  password: 'invalid'
+};
+
+const invalidEmail = {
+  username: 'username',
+  email: 'chopper@anime.com',
+  password: 'invalid'
+};
+
+const nullPassword = {
+  username: 'desola',
+  password: null
+};
+
+describe('ROUTES', () => {
+  after((done) => {
+    models.Users.destroy({ where: { username: 'candy' } }).then(() => done());
+  });
+  describe('POST: (/api/user/signup) - Signup', () => {
+    it('should return a 200 response', (done) => {
+      api.get('/api')
+      .set('Accept', 'application/json')
+      .expect(200, done);
+    });
+    it('should be an object with keys and values', (done) => {
+      api.post('/api/user/signup')
+      .send(chopper)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body).to.have.property('token');
+        expect(res.body.token).to.not.equal(null);
+        expect(res.body).to.have.property('userInfo');
+        expect(res.body.userInfo).to.not.equal(null);
+        done();
+      });
+    });
+    it('should not create another user with same username', (done) => {
+      api.post('/api/user/signup')
+        .send(existingUsername)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.error.text).to.equal('username already in use');
+          done();
+        });
+    });
+    it('should not create another user with same email', (done) => {
+      api.post('/api/user/signup')
+        .send(invalidEmail)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.error.text).to.equal('email already in use');
+          done();
+        });
+    });
+  });
+  describe('POST: (/api/user/signin) - signin', () => {
+    it('should not signin when password is null', (done) => {
+      api.post('/api/user/signin')
+        .send(nullPassword)
+        .end((err, res) => {
+          expect(res.body.token).to.not.exist;
+          expect(res.body.message).to.equal('Incomplete login parameters');
+          done();
+        });
+    });
+    it('should not signin when supplied invalid password', (done) => {
+      api.post('/api/user/signin')
+        .send({
+          username: 'candy',
+          password: 'isewujfikf'
+        })
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body.token).to.not.exist;
+          done();
+        });
+    });
+    it('should not signin username is not supplied', (done) => {
+      api.post('/api/users/signin')
+        .send({
+          password: 'medicine'
+        })
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body.token).to.not.exist;
+          done();
+        });
+    });
+    it('should signin when supplied valid details', (done) => {
+      api.post('/api/user/signin')
+        .send({
+          username: 'candy',
+          password: 'medicine'
+        })
+        .end((err, res) => {
+          expect(res.body).to.have.property('token');
+          expect(res.body.token).to.not.equal(null);
+          expect(res.body).to.have.property('success');
+          expect(res.body.success).to.not.equal(null);
+          expect(res.body.success).to.equal(true);
+          expect(res.body).to.have.property('message');
+          expect(res.body.message).to.not.equal(null);
+          expect(res.body.message).to.equal('You have logged in succesfully');
+          done();
+        });
+    });
+  });
+});
