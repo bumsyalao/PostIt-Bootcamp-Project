29504520467c73f6e1ecd5874b7f0973b4fafcac
bumsyalao <alaobunmi93@gmail.com>
2017-07-26 22:32:19 +0100

    setup react environment

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+ /*./client/components/App.jsx */
+
+import React from 'react';
+
+export default class App extends React.Component{
+  render() {
+    return (
+      <div style ={{textAlign: 'center'}}>
+        <h1>Welcome to Post-It</h1>
+      </div>);
+  }
+}
