1bc01b9d97ad6605a3c6deb1a915cd42932c0488
bumsyalao <alaobunmi93@gmail.com>
2017-08-18 12:44:18 +0100

    change groupCard to es6 style class component

--- client/components/Group/GroupCard.jsx
+++ client/components/Group/GroupCard.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const GroupCard = ({ groupname, id, onClick, joinGroup }) => {
-  console.log(id);
-  return (
-      <div className="card #ffcdd2 red lighten-5">
-        <div className="card-content black-text">
-          <a name={id} onClick={onClick} href="#!" className="card-title">{groupname}</a>
+class GroupCard extends Component {
+  constructor() {
+    super();
+    this.joinGroup = this.joinGroup.bind(this);
+  }
+    joinGroup(){
+      const { id } = this.props;
+      return this.props.joinGroup(id);
+
+    }
+  render() { 
+    const { groupname, id, onClick } = this.props;
+    return (
+        <div className="card #ffcdd2 red lighten-5">
+          <div className="card-content black-text">
+            <a name={id} onClick={onClick} href="#!" className="card-title">{groupname}</a>
+          </div>
+          <div className="card-action">
+            <button
+              className="linkcolor"
+              data-id={id}
+              onClick={this.joinGroup}
+            >
+              This is a link
+            </button>
+          </div>
         </div>
-        <div className="card-action">
-          <button
-            className="linkcolor"
-            data-id={id}
-            onClick={joinGroup}
-          >
-            This is a link
-          </button>
-        </div>
-      </div>
-  );
+    );
+  }
 }
 
 export default GroupCard;
